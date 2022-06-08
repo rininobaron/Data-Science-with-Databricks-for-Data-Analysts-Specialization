3395514f12a6adf5a3d0556b351a645a0595063b
Ricardo Nino de Rivera <rininobaron@gmail.com>
2022-06-08 04:21:58 -0500

    Update HTML_TO_PDF script
    Update the HTML_TO_PDF.py file in order to testing

--- EXTRA_FUNCTION/HTML_TO_PDF.py
+++ EXTRA_FUNCTION/HTML_TO_PDF.py
@@ -22,17 +22,22 @@ target_path = current_path[:-15]
 
 directories = []
 
-print(directories)
-
-for i in 
-
-from pathlib import Path
-import os
  
 rootdir = os.getcwd()
-for path in Path(rootdir).iterdir():
-    if path.is_dir() and :
-        print(path)
+counter_not_git = 0
+counter_target_paths = 0
+for path in os.walk(rootdir):
+    if path[0][87:91] != '.git':
+        counter_not_git += 1
+        if counter_not_git == 0:
+            pass
+        elif path[0][87:101] == 'EXTRA_FUNCTION':
+            pass
+        elif path[0].is_dir():
+            counter_target_paths += 1
+            print(path[0])
+    # if path[1].is_dir():
+    #     print(path)
 
 # for path in directories:
 #     files = os.listdir(path)
